570bb373d9e67f27323377a41847fe063670691d
zhongli <76640596+Zhongli0401@users.noreply.github.com>
2024-05-20 22:57:21 +0530

    push

--- tools/xeno/xeno.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-net file 1>NUL 2>NUL || start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~f0"" %*","","runas",1)(window.close) && exit
-chcp 65001
-
-timeout /t 3 /nobreak > nul
-start "" "xeno\xeno.xpr64"
-
-exit
