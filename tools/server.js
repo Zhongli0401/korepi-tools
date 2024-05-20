5da777a9af3b2229bdbbdcc4a552696d6a362684
zhongli <76640596+Zhongli0401@users.noreply.github.com>
2024-05-20 08:03:53 +0530

    working server

--- tools/server.js
+++ tools/server.js
@@ -268,17 +268,13 @@ const requestListener = function (req, res) {
             updated_by: "StellaR(main) & brother warrior(auth) & EtoShinya(GOat)",
             update_diff: {
                 added_features: [
-                    " https://discord.gg/QwuyRNq4rf join discord for more info",
-                    "Fix camera issues",
-					"by FAd#1234",
-					"fix 1.3.1.3.v3",
+                    "https://discord.gg/QwuyRNq4rf join discord for more info",
+					"By FAd#1234",
 					"if u face error 409 put your old  cfg.ini cfg.json imgui.ini in korepi folder",
 					"help at bkebi server",
-					"Restored all working features."
                 ],
-                deleted_features: [
-                    "修复所有失效功能",
-                    "Restore all malfunctioning features",
+                extra_features: [
+				    "fix 1.3.1.3.v3",
 					"Removed AUTH v3."				
                 ],
                 total_size: "76.0 MB"
